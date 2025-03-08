c1acfac4a3e83ccd965a8b7d281ea434c19c6d1a
raurabryanclear <bryanroura.clearminds@gmail.com>
2025-03-08 14:21:28 -0500

    de

--- src/controller/ventas.controller.js
+++ src/controller/ventas.controller.js
@@ -3,7 +3,7 @@ import { pool } from "../db.js";
 export const getSales = async (req, res) => {
     try {
         
-        const {rows} = await pool.query("    SELECT po.precio, ve.cantidad, (po.precio*ve.cantidad) as total FROM producto po, ventas ve WHERE po.id_producto = ve.id ");
+        const {rows} = await pool.query(" SELECT ve.id, ve.id_cliente, ve.id_producto, ve.cantidad, po.precio, (po.precio*ve.cantidad) as total,  ve.fecha_compra FROM producto po, ventas ve  WHERE po.id_producto = ve.id ");
         return res.json(rows);
     } catch (error) {
         console.log(error.message);
